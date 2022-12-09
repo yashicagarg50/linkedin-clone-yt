004b28212d91875823160b3b2aeb996df7f18e0b
yashicagarg50 <yashicagarg50@gmail.com>
2022-12-10 03:03:13 +0530

    changes

--- linkedinin-clone-yt/src/Header.js
+++ linkedinin-clone-yt/src/Header.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import './Header.css'
+import HeaderOption from './HeaderOption';
 import SearchIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/Search';
+import HomeIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/Home';
+import SupervisorAccountIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/BusinessCenter';
+import ChatIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/Chat';
+import NotificationsIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/Notifications';
+
 function Header() {
   return (
-    <div className="Header">
-      <h1>This is the header</h1>
+    <div className="header">
+     
 
       <div className="header__left">
-        <img src='' alt=''></img>
+        <img src='linkedin.jpg' />
 
         <div className="header__search">
             <SearchIcon />
             <input type="text" />
         </div>
-
       </div>
 
       <div className="header__right">
-
-
+        <HeaderOption Icon ={HomeIcon} title='Home'/>
+        <HeaderOption Icon ={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon ={BusinessCenterIcon} title='Jobs'/>
+        <HeaderOption Icon ={ChatIcon} title='Messaging'/>
+        <HeaderOption Icon ={NotificationsIcon} title='Notification'/>
+        <HeaderOption avatar ="./me.jpg" title='me' />
       </div>
     
     
