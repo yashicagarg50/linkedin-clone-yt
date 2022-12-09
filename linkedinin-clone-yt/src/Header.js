2287cefd6e01ded4d2f0b0e0ce97cc11395f1c0b
yashicagarg50 <yashicagarg50@gmail.com>
2022-12-10 04:12:16 +0530

    changes

--- linkedinin-clone-yt/src/Header.js
+++ linkedinin-clone-yt/src/Header.js
@@ -14,7 +14,7 @@ function Header() {
      
 
       <div className="header__left">
-        <img src="./linkedin.jpg" alt="" />
+        <img src=".linkedinin-clone-yt/src/linkedin.jpg" alt="" />
 
         <div className="header__search">
             <SearchIcon />
@@ -28,7 +28,7 @@ function Header() {
         <HeaderOption Icon ={BusinessCenterIcon} title='Jobs'/>
         <HeaderOption Icon ={ChatIcon} title='Messaging'/>
         <HeaderOption Icon ={NotificationsIcon} title='Notification'/>
-        <HeaderOption avatar ="./me.jpg" title='Me' />
+        <HeaderOption avatar ="linkedinin-clone-yt/src/me.jpg" title='Me' />
       </div>
     
     
--- linkedinin-clone-yt/src/Sidebar.js
+++ linkedinin-clone-yt/src/Sidebar.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 import { Avatar } from '@material-ui/core';
 function Sidebar() {
+  const recentItem = (topic) => {
+    <div className="sidebar__recentItem">
+        <span className="sidebar__hash">
+          <p>(topic)</p>   
+        </span>
+    </div>
+  }
   return (
     <div className='sidebar'>
       <div className="sidebar__top">
-        <img src="./bg.jpg" alt="" />
+        <img src="linkedinin-clone-yt/src/bg.jpg" alt="" />
         <Avatar className="sidebar__avatar" />
         <h2>Yashica Garg</h2>
         <h4>yashicagarg50@gmail.com</h4>
@@ -22,11 +29,16 @@ function Sidebar() {
         </div>
       </div>
 
-      <div className="sidebar__button">
+      <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
       </div>
     </div>
   )
-}
+};
 
 export default Sidebar
