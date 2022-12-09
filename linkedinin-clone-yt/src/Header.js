e796ff1894807b7fed55edc6be38d79a696e0910
yashicagarg50 <yashicagarg50@gmail.com>
2022-12-10 00:43:26 +0530

    changes

---
+++ linkedinin-clone-yt/src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+// import SearchIcon from 'd:/GitHub/linkedin-clone-yt/node_modules/@material-ui/icons/Search';
+function Header() {
+  return (
+    <div className="Header">
+      <h1>This is the header</h1>
+
+      <div className="header__left">
+        <img src='' alt=''></img>
+
+        <div className="header__search">
+            {/* <SearchIcon /> */}
+            <input type="text" />
+        </div>
+
+      </div>
+
+      <div className="header__right">
+
+
+      </div>
+    
+    
+
+    </div>
+  )
+}
+
+export default Header
